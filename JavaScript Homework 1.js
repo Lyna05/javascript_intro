f545e45f32f3ea33232ea759d141014e9c4d5466
Lyna05 <lyne_armelle@hotmail.com>
2024-03-15 14:56:17 +0100

    Adding the Javascript Homework1.js.

---
+++ JavaScript Homework 1.js
@@ -0,0 +1,65 @@
+let lieblingsBuch = "Bodyguard"; 
+console.log(lieblingsBuch);
+
+let anzahlGeschwister = 3;
+console.log(anzahlGeschwister);
+
+let hatFuehrerschein = true;
+console.log(hatFuehrerschein);
+
+let keinFuehrerschein = false;
+console.log(keinFuehrerschein);
+
+let momentaneStimmung = null;
+console.log(momentaneStimmung);
+
+let naechsteReiseziel;
+console.log(naechsteReiseziel);
+
+let lieblingsFarbe = "Blau";
+console.log(lieblingsFarbe);
+
+let wohnflaeche = 100;
+console.log(wohnflaeche);
+
+let magKaffee = true;
+console.log(magKaffee);
+
+let magkeinKaffee = false;
+console.log(magkeinKaffee);
+
+let aktuellesProjekt = null;
+console.log(aktuellesProjekt);
+
+let letztesAbendessen;
+console.log(letztesAbendessen);
+
+let Geburtsstadt = "Duala";
+console.log(Geburtsstadt);
+
+let kaffeetassenProTag = 2;
+console.log(kaffeetassenProTag);
+
+let istVegetarier = false;
+console.log(istVegetarier);
+
+let zukunftsWunsch = null;
+console.log(zukunftsWunsch);
+
+let lieblingsSportart;
+console.log(lieblingsSportart);
+
+let lieblingsFilm = "Prettywoman";
+console.log(lieblingsFilm);
+
+let hoeheInCM = 172;
+console.log(hoeheInCM);
+
+let hatHaustier = false;
+console.log(hatHaustier);
+
+let mittagessenIdee = null;
+console.log(mittagessenIdee);
+
+let lieblingsMusikband;
+console.log(lieblingsMusikband);
