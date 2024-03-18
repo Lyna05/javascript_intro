61e4ada18662b4b4fa8b2909c0c951bd7896d485
Lyna05 <lyne_armelle@hotmail.com>
2024-03-18 11:07:35 +0100

    Adding maths examples. Adding calculator code.

---
+++ day2/maths.js
@@ -0,0 +1,36 @@
+// Addition
+console.log(10 + 5);
+// Subtraktion
+console.log(30 - 5);
+// Multiplikation
+console.log(4 * 5);
+// Division
+console.log(18 / 2);
+console.log(18 / 5);
+// Modulo
+console.log(18 % 5);
+
+// Praezendenz
+console.log(10 + 5 * 2);
+console.log((10 + 5) * 2);
+
+let x = 10;
+let y = 2;
+
+let sum;
+sum = x + y;
+console.log("The sum of x and y is: " + sum);
+
+let result;
+result = x -y;
+console.log("The result of subtraction is: " + result);
+
+result = x * y;
+console.log("The result of multiplication is: " + result);
+
+result = x / y;
+console.log("The result of division is: " + result);
+
+result = x % y;
+console.log("The result of modulo division is: " + result);
+
---
+++ day2/parse_numbers.js
@@ -0,0 +1,16 @@
+const inputString1 = "123";
+const inputString2 = "456";
+//console.log(inputString1 + inputString2);
+
+const inputNumber1 = parseInt(inputString1);
+const inputNumber2 = parseInt(inputString2);
+//console.log(inputNumber1 + inputNumber2);
+
+const inputString3 = "1.23";
+const inputString4 = "4.56";
+//console.log(inputString3 + inputString4);
+
+const inputNumber3 = parseInt(inputString3);
+const inputNumber4 = parseInt(inputString4);
+console.log(inputNumber3);
+console.log(inputNumber4);
