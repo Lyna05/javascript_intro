db5744e22360e0211ac0d512548ebbd77b11e31c
Lyna05 <lyne_armelle@hotmail.com>
2024-03-19 13:07:54 +0100

    Adding for loop into javascript example.

---
+++ day3/for_loops_intro.js
@@ -0,0 +1,12 @@
+const numbersArray = [1, 2, 3, 4, 5, 6 ,7, 8];
+
+for (let i = 0; i < numbersArray.length; i++) {
+    console.log(numbersArray[i]);
+}
+
+for (let i = numbersArray.length - 1; i >=0; i--) {
+    console.log(numbersArray[i]);
+}
+
+
+    
